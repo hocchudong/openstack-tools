dfac4a5b4663603374a635a5bf2aad5cd1f5b5d7
Cong To <tcvn1985@gmail.com>
2017-08-14 11:47:24 +0700

    dinh dang script
    dinh dang script

--- scripts/OpenStack-Newton-No-HA/noha_ctl_prepare.sh
+++ scripts/OpenStack-Newton-No-HA/noha_ctl_prepare.sh
@@ -184,8 +184,8 @@ install_memcached
 echocolor "Dat hostname cho cac may"
 sleep 5
 hostnamectl set-hostname $CTL1_HOSTNAME
-ssh root@$COM1_IP_NIC1  "hostnamectl set-hostname $COM1_HOSTNAME"
-ssh root@$COM2_IP_NIC1  "hostnamectl set-hostname $COM2_HOSTNAME"
+ssh root@$COM1_IP_NIC1 "hostnamectl set-hostname $COM1_HOSTNAME"
+ssh root@$COM2_IP_NIC1 "hostnamectl set-hostname $COM2_HOSTNAME"
 
 echocolor "XONG & KHOI DONG LAI MAY CHU"
 sleep 5

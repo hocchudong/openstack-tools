29b38ef7a7e697d0087dfbf870243c919f573a5e
Tô Thành Công <tcvn1985@gmail.com>
2021-09-24 09:31:17 +0700

    update file

--- scripts/OpenStack-Victoria-No-HA/Ubuntu/com1_01_env.sh
+++ scripts/OpenStack-Victoria-No-HA/Ubuntu/com1_01_env.sh
@@ -6,33 +6,33 @@ source config.cfg
 
 # Function update and upgrade for COMPUTE
 function update_upgrade () {
-	echocolor "Update and Upgrade COMPUTE"
-	sleep 3
-	apt-get update -y && apt-get upgrade -y
+  echocolor "Update and Upgrade COMPUTE"
+  sleep 3
+  apt-get update -y && apt-get upgrade -y
 }
 
 
 # Function install and config NTP
 function install_ntp () {
-	echocolor "Install NTP"
-	sleep 3
+  echocolor "Install NTP"
+  sleep 3
 
-	apt-get install chrony -y
-	ntpfile=/etc/chrony/chrony.conf
+  apt-get install chrony -y
+  ntpfile=/etc/chrony/chrony.conf
 
-	sed -i 's|'"pool 2.debian.pool.ntp.org offline iburst"'| \
-	'"server $HOST_CTL iburst"'|g' $ntpfile
+  sed -i 's|'"pool 2.debian.pool.ntp.org offline iburst"'| \
+  '"server $HOST_CTL iburst"'|g' $ntpfile
 
-	service chrony restart
+  service chrony restart
 }
 
 # Function install OpenStack packages (python-openstackclient)
 function install_ops_packages () {
-	echocolor "Install OpenStack client"
-	sleep 3
-	sudo apt-get install software-properties-common -y 2>&1 | tee -a filelog-install.txt
+  echocolor "Install OpenStack client"
+  sleep 3
+  sudo apt-get install software-properties-common -y 2>&1 | tee -a filelog-install.txt
   sudo add-apt-repository cloud-archive:wallaby -y 2>&1 | tee -a filelog-install.txt
-	sudo echo "deb http://172.16.70.131:8081/repository/u20wallaby/ focal-updates/wallaby main" > cloudarchive-wallaby.list
+  sudo echo "deb http://172.16.70.131:8081/repository/u20wallaby/ focal-updates/wallaby main" > cloudarchive-wallaby.list
   sudo apt-get update -y 2>&1 | tee -a filelog-install.txt
   sudo apt-get upgrade -y 2>&1 | tee -a filelog-install.txt
   sudo apt-get install python3-openstackclient -y 2>&1 | tee -a filelog-install.txt

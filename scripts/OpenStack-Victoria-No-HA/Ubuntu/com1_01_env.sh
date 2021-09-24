9b64de8ed663603f853ee598909cfdc1dd76de89
Tô Thành Công <tcvn1985@gmail.com>
2021-09-24 17:18:25 +0700

    Update com1_01_env.sh

--- scripts/OpenStack-Victoria-No-HA/Ubuntu/com1_01_env.sh
+++ scripts/OpenStack-Victoria-No-HA/Ubuntu/com1_01_env.sh
@@ -35,7 +35,7 @@ function install_ops_packages () {
   sudo apt-get install software-properties-common -y 2>&1 | tee -a filelog-install.txt
   sudo add-apt-repository cloud-archive:wallaby -y 2>&1 | tee -a filelog-install.txt
   
-  sudo echo "deb http://172.16.70.131:8081/repository/u20wallaby/ focal-updates/wallaby main" > cloudarchive-wallaby.list
+  sudo echo "deb http://172.16.70.131:8081/repository/u20wallaby/ focal-updates/wallaby main" >  /etc/apt/sources.list.d/cloudarchive-wallaby.list
 
   sudo apt-get update -y 2>&1 | tee -a filelog-install.txt
   sudo apt-get upgrade -y 2>&1 | tee -a filelog-install.txt

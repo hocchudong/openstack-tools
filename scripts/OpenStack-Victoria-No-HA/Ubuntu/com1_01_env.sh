54f772722f9d1e076b4643c57b1af61eb374dfac
Tô Thành Công <tcvn1985@gmail.com>
2021-09-24 08:52:08 +0700

    updae file

--- scripts/OpenStack-Victoria-No-HA/Ubuntu/com1_01_env.sh
+++ scripts/OpenStack-Victoria-No-HA/Ubuntu/com1_01_env.sh
@@ -32,6 +32,7 @@ function install_ops_packages () {
 	sleep 3
 	sudo apt-get install software-properties-common -y 2>&1 | tee -a filelog-install.txt
   sudo add-apt-repository cloud-archive:wallaby -y 2>&1 | tee -a filelog-install.txt
+	sudo echo "deb http://172.16.70.131:8081/repository/u20wallaby/ focal-updates/wallaby main" > cloudarchive-wallaby.list
   sudo apt-get update -y 2>&1 | tee -a filelog-install.txt
   sudo apt-get upgrade -y 2>&1 | tee -a filelog-install.txt
   sudo apt-get install python3-openstackclient -y 2>&1 | tee -a filelog-install.txt

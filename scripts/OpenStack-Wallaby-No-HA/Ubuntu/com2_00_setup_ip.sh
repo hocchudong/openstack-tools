c68e9aef87e8f5e8dcca8f18ffc041f6d753960f
Tô Thành Công <tcvn1985@gmail.com>
2021-10-04 15:51:02 +0700

    update file

---
+++ scripts/OpenStack-Wallaby-No-HA/Ubuntu/com2_00_setup_ip.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Author: HOC CHU DONG
+source function.sh
+source config.cfg
+
+# Function config hostname
+function config_hostname () {
+echo "$COM1_HOSTNAME" > /etc/hostname
+echo "127.0.0.1 locahost $COM1_HOSTNAME" > /etc/hosts
+echo "$CTL1_IP_NIC2 $CTL1_HOSTNAME" >> /etc/hosts
+echo "$COM1_IP_NIC2 $COM1_HOSTNAME" >> /etc/hosts
+echo "$COM2_IP_NIC2 $COM2_HOSTNAME" >> /etc/hosts
+echo "$CINDER1_IP_NIC2 $CINDER1_HOSTNAME" >> /etc/hosts
+}
+
+# Function IP address
+function config_ip () {
+
+cat << EOF > /etc/network/interfaces
+# loopback network interface
+auto lo
+iface lo inet loopback
+
+# DATA VM network
+auto eth1
+iface eth1 inet static
+address $COM1_IP_NIC1
+netmask $NETMASK_NIC1
+
+### API
+auto eth2
+iface eth2 inet static
+address $COM1_IP_NIC2
+netmask $NETMASK_NIC2
+gateway $GATAWAY_NIC2
+dns-nameservers 8.8.8.8
+
+# Provider Network
+# MGNT
+auto eth3
+iface eth3 inet static
+address $COM1_IP_NIC3
+netmask $NETMASK_NIC3
+EOF
+}
+
+#######################
+###Execute functions###
+#######################
+
+# Config CONTROLLER node
+echocolor "Config $COM1_HOSTNAME node"
+sleep 3
+
+## Config hostname
+config_hostname
+
+## IP address
+config_ip
+
+echocolor "Reboot $COM1_HOSTNAME node"
+init 6
+

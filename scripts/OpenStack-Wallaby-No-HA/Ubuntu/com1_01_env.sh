b02ef800f9c2a1ff4f852212a7a4e09279ae3cbf
Tô Thành Công <tcvn1985@gmail.com>
2021-10-05 14:27:19 +0700

    update file, bo cinder

--- scripts/OpenStack-Wallaby-No-HA/Ubuntu/com1_01_env.sh
+++ scripts/OpenStack-Wallaby-No-HA/Ubuntu/com1_01_env.sh
@@ -13,7 +13,6 @@ function config_hostname () {
   echo "$CTL1_IP_NIC2 $CTL1_HOSTNAME" > /etc/hosts
   echo "$COM1_IP_NIC2 $COM1_HOSTNAME" >> /etc/hosts
   echo "$COM2_IP_NIC2 $COM2_HOSTNAME" >> /etc/hosts
-  echo "$CINDER1_IP_NIC2 $CINDER1_HOSTNAME" >> /etc/hosts
 
   echo "127.0.0.1 $COM1_HOSTNAME" >> /etc/hosts
   echo "127.0.0.2 locahost" >> /etc/hosts
--- scripts/OpenStack-Wallaby-No-HA/Ubuntu/ctl_01_env.sh
+++ scripts/OpenStack-Wallaby-No-HA/Ubuntu/ctl_01_env.sh
@@ -14,7 +14,6 @@ function config_hostname () {
   echo "$CTL1_IP_NIC2 $CTL1_HOSTNAME" > /etc/hosts
   echo "$COM1_IP_NIC2 $COM1_HOSTNAME" >> /etc/hosts
   echo "$COM2_IP_NIC2 $COM2_HOSTNAME" >> /etc/hosts
-  echo "$CINDER1_IP_NIC2 $CINDER1_HOSTNAME" >> /etc/hosts
   
   echo "127.0.0.1 $CTL1_HOSTNAME" >> /etc/hosts
   echo "127.0.0.2 localhost" >> /etc/hosts

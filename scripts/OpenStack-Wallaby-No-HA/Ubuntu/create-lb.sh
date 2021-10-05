c07158d78820fe2960e8fb98e6c3a74248eef388
Tô Thành Công <tcvn1985@gmail.com>
2021-10-05 23:43:45 +0700

    Update create-lb.sh
    update

--- scripts/OpenStack-Wallaby-No-HA/Ubuntu/create-lb.sh
+++ scripts/OpenStack-Wallaby-No-HA/Ubuntu/create-lb.sh
@@ -9,3 +9,17 @@ source /root/admin-openrc
 
 openstack loadbalancer create --name lb01 --vip-subnet-id selfservice
 
+
+openstack loadbalancer listener create --name listener01 --protocol TCP --protocol-port 80 lb01
+
+
+openstack loadbalancer pool create --name pool01 --lb-algorithm ROUND_ROBIN --listener listener01 --protocol TCP
+
+openstack loadbalancer member create --subnet-id private-subnet --address 192.168.100.43 --protocol-port 80 pool01
+
+openstack loadbalancer member create --subnet-id private-subnet --address 192.168.100.132 --protocol-port 80 pool01
+
+openstack loadbalancer member list pool01
+
+
+openstack floating ip create public

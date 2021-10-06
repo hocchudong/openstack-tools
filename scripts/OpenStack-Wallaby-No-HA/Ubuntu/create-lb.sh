ac587564ad17e098bd43c2ad93884d004b2976b0
Tô Thành Công <tcvn1985@gmail.com>
2021-10-06 09:23:28 +0700

    Update create-lb.sh

--- scripts/OpenStack-Wallaby-No-HA/Ubuntu/create-lb.sh
+++ scripts/OpenStack-Wallaby-No-HA/Ubuntu/create-lb.sh
@@ -10,6 +10,7 @@ source /root/admin-openrc
 
 wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img
 
+echocolor "Tao image Ubuntu 18"
 openstack image create --disk-format qcow2 --container-format bare \
   --public --file ./bionic-server-cloudimg-amd64.img bionic-server-cloudimg-amd64
 
@@ -20,17 +21,15 @@ ssh-keygen -N "" -f /root/.ssh/id_rsa
 
 openstack keypair create --public-key ~/.ssh/id_rsa.pub controller-key
 
-openstack security group create allow-all-traffic --description 'Allow All Ingress Traffic'
-openstack security group rule create --protocol icmp allow-all-traffic
-openstack security group rule create --protocol tcp  allow-all-traffic
-openstack security group rule create --protocol udp  allow-all-traffic
+ID_ADMIN_PROJECT=`openstack project list | grep admin | awk '{print $2}'`
+ID_SECURITY_GROUP=`openstack security group list | grep $ID_ADMIN_PROJECT | awk '{print $2}'`
 
-
+  
 sleep 15
 openstack server create --flavor small \
   --image bionic-server-cloudimg-amd64 \
   --key-name controller-key \
-  --security-group allow-all-traffic \
+  --security-group $ID_SECURITY_GROUP \
   --network sub-selfservice \
   ubuntu01
 
@@ -38,24 +37,27 @@ sleep 15
 openstack server create --flavor small \
   --image bionic-server-cloudimg-amd64 \
   --key-name controller-key \
-  --security-group allow-all-traffic \
+  --security-group $ID_SECURITY_GROUP\
   --network sub-selfservice \
   ubuntu02
 
 sleep 30
+echocolor "Tao LB"
 openstack loadbalancer create --name lb01 --vip-subnet-id sub-selfservice
 
 sleep 120
+echocolor "Tao listener cho LB"
 openstack loadbalancer listener create --name listener01 --protocol TCP --protocol-port 80 lb01
 
 sleep 30
+echocolor "Tao pool cho LB"
 openstack loadbalancer pool create --name pool01 --lb-algorithm ROUND_ROBIN --listener listener01 --protocol TCP
 
 
 IP_VM01=`openstack server list | egrep ubuntu01 | awk '{print $8}' | awk -F= '{print $2}'`
 IP_VM02=`openstack server list | egrep ubuntu02 | awk '{print $8}' | awk -F= '{print $2}'`
 
-
+echocolor "Gan member vao pool cho LB"
 sleep 30
 openstack loadbalancer member create --subnet-id sub-selfservice --address $IP_VM01 --protocol-port 80 pool01
 
@@ -63,7 +65,8 @@ sleep 30
 openstack loadbalancer member create --subnet-id sub-selfservice --address $IP_VM02 --protocol-port 80 pool01
 
 sleep 30
+echocolor "Liet ke member cua LB"
 openstack loadbalancer member list pool01
 
-
+echocolor "Cap floating IP cho LB"
 openstack floating ip create public

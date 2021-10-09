5fd503817e400994a9f6512d6b3a011fa4e919b5
Tô Thành Công <tcvn1985@gmail.com>
2021-10-09 22:27:41 +0700

    update file

--- scripts/OpenStack-Victoria-No-HA/Ubuntu/ctl_01_env.sh
+++ scripts/OpenStack-Victoria-No-HA/Ubuntu/ctl_01_env.sh
@@ -52,7 +52,7 @@ function install_ops_packages () {
 	sleep 3
 	sudo apt-get install software-properties-common -y 2>&1 | tee -a filelog-install.txt
   sudo add-apt-repository cloud-archive:victoria -y 2>&1 | tee -a filelog-install.txt
-  sudo echo "deb http://172.16.70.131:8081/repository/u20wallaby/ focal-updates/victoria main" > /etc/apt/sources.list.d/cloudarchive-victoria.list
+  sudo echo "deb http://172.16.70.131:8081/repository/u20victoria/ focal-updates/victoria main" > /etc/apt/sources.list.d/cloudarchive-victoria.list
   
   sudo apt update -y 2>&1 | tee -a filelog-install.txt
   sudo apt upgrade -y 2>&1 | tee -a filelog-install.txt
